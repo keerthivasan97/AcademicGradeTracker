c0644c4b011e925c13764f368ccb3534f98964e7
keerthivasan97 <kerthivasan.s2024@vitstudent.ac.in>
2026-06-18 01:32:35 +0530

    i have updated the sgpa predictor and accomplished all the targets

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 import storage
 
@@ -30,12 +31,94 @@ def adds(sem,sgpa, credit, username):
     else:
         messagebox.showinfo("Skipped", "Semester data skipped due to invalid input.")
 
+def calcul(arr):
+    grade_p = {
+        "S":10,
+        "A":9,
+        "B":8,
+        "C":7,
+        "D":6,
+        "E":5,
+        "F":0
+    }
+    total_point=0
+    total_credit=0
+    for g,c in arr:
+        grade = g.get()
+        credit=c.get()
+        if grade == "--select--" or credit == "--select--":
+            continue
+        credit = float(credit)
+        point = grade_p[grade]
+
+        total_point += point*credit
+        total_credit +=credit
+
+    if total_credit == 0:
+        messagebox.showerror("Error", "Please select at least one subject.")
+        return
+
+    sgpa = total_point / total_credit
+    messagebox.showinfo("Predicted SGPA", f"Predicted SGPA: {sgpa:.2f}")
+
+def select(event):
+    combo = event.widget
+    print(combo.get())
+
 def pre_sgpa(username):
-    
-    pass
+    global Cgpa,view,add_sem,grade
+
+    if grade is None:
+        grade = tk.Frame(root)
+
+        if add_sem is not None:
+            add_sem.pack_forget()
+            for widget in add_sem.winfo_children():
+                widget.destroy()
+            add_sem = None
+
+        if view is not None:
+            view.pack_forget()
+            for widget in view.winfo_children():
+                widget.destroy()
+            view = None
+
+        if Cgpa is not None:
+            Cgpa.pack_forget()
+            for widget in Cgpa.winfo_children():
+                widget.destroy()
+            Cgpa = None
+        arr = []
+
+        for i in range(10):
+            tk.Label(grade,text="grade").grid(row=i,column=0)
+
+            g = ttk.Combobox(grade,values=["--select--","S","A", "B", "C","D","E","F"],state="readonly")
+            g.grid(row=i,column=1)
+            g.set("--select--")
+            g.bind("<<ComboboxSelected>>", select)
+
+            tk.Label( grade,text="credit").grid(row=i,column=2)
+
+            c = ttk.Combobox(grade,values=["--select--","1", "1.5", "2","3","4","5"],state="readonly")
+            c.grid(row=i,column=3)
+            c.set("--select--")
+            c.bind("<<ComboboxSelected>>", select)
+
+            arr.append([g,c])
+            
+
+        btn = tk.Button(grade,text = "calculate",command=lambda:calcul(arr))
+        btn.grid(row=11,column=2)
+        grade.pack()
+    else:
+        grade.pack()
+
+
+
 
 def calculate_cgpa(username):
-    global Cgpa,view,add_sem
+    global Cgpa,view,add_sem,grade
 
     if Cgpa is None:
         if add_sem is not None:
@@ -43,6 +126,11 @@ def calculate_cgpa(username):
             for widget in add_sem.winfo_children():
                 widget.destroy()
             add_sem = None
+        if grade is not None:
+            grade.pack_forget()
+            for w in grade.winfo_children():
+                w.destroy()
+            grade = None
 
         if view is not None:
             view.pack_forget()
@@ -62,14 +150,14 @@ def calculate_cgpa(username):
                 tot_cre += float(data['credit'])
                 tot_s += float(data['credit'])*float(data['gpa'])
             cgpa = float(tot_s/tot_cre)
-            label = tk.Label(Cgpa,text = f"CGPA: {cgpa}", font= ("Arial",20))
+            label = tk.Label(Cgpa,text = f"CGPA: {cgpa:.2f}", font= ("Arial",20))
             label.grid(row=0,column=0,padx=20,pady=20)
             Cgpa.pack()
     else:
         Cgpa.pack()
 
 def view_semester_data(username):
-    global view,add_sem,Cgpa
+    global view,add_sem,Cgpa,grade
     if view is None:
 
         if add_sem is not None:
@@ -77,7 +165,13 @@ def view_semester_data(username):
             for widget in add_sem.winfo_children():
                 widget.destroy()
             add_sem = None
-
+        
+        if grade is not None:
+            grade.pack_forget()
+            for w in grade.winfo_children():
+                w.destroy()
+            grade = None
+            
         if Cgpa is not None:
             Cgpa.pack_forget()
             for widget in Cgpa.winfo_children():
@@ -109,7 +203,7 @@ def view_semester_data(username):
 
 
 def add_semester_data(username):
-    global add_sem, view,Cgpa
+    global add_sem, view,Cgpa,grade
 
     if add_sem is None:
 
@@ -119,6 +213,12 @@ def add_semester_data(username):
                 widget.destroy() # Clear the view frame content
             view = None
 
+        if grade is not None:
+            grade.pack_forget()
+            for w in grade.winfo_children():
+                w.destroy()
+            grade = None
+            
         if Cgpa is not None:
             Cgpa.pack_forget()
             for widget in Cgpa.winfo_children():
@@ -178,5 +278,5 @@ def show_dashboard(main_root,username):
 root = None
 add_sem = None
 view = None
-Grade = None
+grade = None
 Cgpa = None
